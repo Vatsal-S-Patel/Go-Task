e4bf2dbd75f77e232d1a234d2bb7f11f3acc9358
Vatsal Patel <vatsalspatel007@gmail.com>
2024-03-19 17:16:24 +0530

    Use goroutine to read CSV and insert in database using WaitGroup

--- Task-11/backend/helper/dumpDataFromCsvToDb.go
+++ Task-11/backend/helper/dumpDataFromCsvToDb.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"train-task/database"
@@ -34,13 +35,16 @@ func DumpDataFromCsvToDb(client *mongo.Client, csvFileName string) error {
 	// Get Train Collection from database
 	trainsCollection := database.GetCollection(client, "trains")
 
-	for {
-		// TODO: wrap it into Go func
+	// Use WaitGroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
 
+	for {
 		// Read the next row, if csv file is not ends or any error is not occurred
 		row, err := csvReader.Read()
 		if err != nil {
 			if err == io.EOF {
+				// Wait till all goroutine executed
+				wg.Wait()
 				log.Println("INFO: csv file end")
 				return nil
 			}
@@ -48,36 +52,44 @@ func DumpDataFromCsvToDb(client *mongo.Client, csvFileName string) error {
 			return err
 		}
 
-		// convert trainId and trainNo into int from string
-		trainId, err := strconv.Atoi(row[0])
-		if err != nil {
-			log.Println("ERROR: while parsing int")
-			return err
-		}
-		trainNo, err := strconv.Atoi(row[1])
-		if err != nil {
-			log.Println("ERROR: while parsing int")
-			return err
-		}
+		// Increment the WaitGroup counter
+		wg.Add(1)
 
-		// train is the struct of Train made from CSV row
-		train := model.Train{
-			Sno:         trainId,
-			No:          trainNo,
-			Name:        row[2],
-			Source:      row[3],
-			Destination: row[4],
-		}
+		// a goroutine to process the row
+		go func(row []string) {
+			// Defer the wg.Done()
+			defer wg.Done()
 
-		// Defining context that run on background for 10 seconds
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+			// convert trainId and trainNo into int from string
+			trainId, err := strconv.Atoi(row[0])
+			if err != nil {
+				log.Println("ERROR: while parsing int")
+				return
+			}
+			trainNo, err := strconv.Atoi(row[1])
+			if err != nil {
+				log.Println("ERROR: while parsing int")
+				return
+			}
 
-		// Insert that train in database
-		_, err = trainsCollection.InsertOne(ctx, train)
-		if err != nil {
-			log.Println("ERROR: while inserting train in database", err.Error())
-		}
+			// train is the struct of Train made from CSV row
+			train := model.Train{
+				Sno:         trainId,
+				No:          trainNo,
+				Name:        row[2],
+				Source:      row[3],
+				Destination: row[4],
+			}
+
+			// Defining context that run on background for 10 seconds
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+
+			// Insert that train in database
+			_, err = trainsCollection.InsertOne(ctx, train)
+			if err != nil {
+				log.Println("ERROR: while inserting train in database", err.Error())
+			}
+		}(row)
 	}
-
 }
